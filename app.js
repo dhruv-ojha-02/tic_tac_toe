2bc9a7fbff283b82e3a99e0ae7d37ffe58afbd68
dhruv-ojha-02 <dhruvojha02@gmail.com>
2024-02-04 23:41:08 +0530

    first changes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ let msg=document.querySelector("#msg");
 let turnO= true;
 let count=0;    //to track draw 
 
+let dhruv=10;
+
 const winPatterns=[[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
 
 const resetGame = () =>{
